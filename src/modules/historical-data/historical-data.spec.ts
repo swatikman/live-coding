3ec5bc0c3c59aaeff39ef916af0f0d00bdcd1b50
Sviatoslav Chyzh <sviatoslav.chyzh@idealogic.dev>
2024-09-25 09:32:31 +0200

    Added more tests

--- src/modules/historical-data/historical-data.spec.ts
+++ src/modules/historical-data/historical-data.spec.ts
@@ -14,7 +14,7 @@ describe('Historical data service', () => {
 
   describe('function analyzeHistoricalData', () => {
     describe('fails', () => {
-      it('should fail, fetchKLines method got rejected', async () => {
+      it('fetchKLines method got rejected', async () => {
         jest.spyOn(binanceService, 'fetchKLines').mockImplementation(async () => Promise.reject('Wrong ACCESS_KEY'));
 
         await expect(analyzeHistoricalData({
@@ -25,10 +25,10 @@ describe('Historical data service', () => {
     })
 
     describe('successful', () => {
-      it('should be successful, fetchKLines returned valid data, price increase', async () => {
+      it('fetchKLines returned valid data, price increase', async () => {
         const kLines: BinanceKLine[] = [
-          [1725451200000,"50000.00000000","50000.99000000","50000.95000000","50000.00000000","776.26939000",1725454799999,"43949985.04994390",93706,"363.62337000","20590941.14061460","0"],
-          [1725454800000,"60000.00000000","60000.00000000","60000.41000000","60000.00000000","1446.03410000",1725458399999,"81687899.57771600",192315,"740.04595000","41812275.07135860","0"]
+          [1725451200000,"50000.00000000","50000.00000000","50000.00000000","50000.00000000","776.26939000",1725454799999,"43949985.04994390",93706,"363.62337000","20590941.14061460","0"],
+          [1725454800000,"60000.00000000","60000.00000000","60000.00000000","60000.00000000","1446.03410000",1725458399999,"81687899.57771600",192315,"740.04595000","41812275.07135860","0"]
         ];
 
         jest.spyOn(binanceService, 'fetchKLines').mockImplementation(async () => Promise.resolve(kLines));
@@ -45,6 +45,52 @@ describe('Historical data service', () => {
           priceChangeDirection: 'increase',
           priceChange: 10000,
           priceChangeInPercent: 20
+        })
+      })
+
+      it('fetchKLines returned valid data, price decrease', async () => {
+        const kLines: BinanceKLine[] = [
+          [1725451200000,"100000.00000000","100000.00000000","100000.00000000","100000.00000000","776.26939000",1725454799999,"43949985.04994390",93706,"363.62337000","20590941.14061460","0"],
+          [1725454800000,"50000.00000000","50000.00000000","50000.00000000","50000.00000000","1446.03410000",1725458399999,"81687899.57771600",192315,"740.04595000","41812275.07135860","0"]
+        ];
+
+        jest.spyOn(binanceService, 'fetchKLines').mockImplementation(async () => Promise.resolve(kLines));
+        const result = await analyzeHistoricalData({
+          symbol: BinanceSymbolEnum.BTC_USDT,
+          interval: BinanceIntervalEnum.ONE_HOUR,
+        })
+
+        expect(result).toEqual({
+          symbol: 'BTCUSDT',
+          interval: '1h',
+          timestampFrom: 1725451200000,
+          timestampTo: 1725458399999,
+          priceChangeDirection: 'decrease',
+          priceChange: -50000,
+          priceChangeInPercent: -50
+        })
+      })
+
+      it('fetchKLines returned valid data, price remains equal', async () => {
+        const kLines: BinanceKLine[] = [
+          [1725451200000,"50000.00000000","50000.00000000","50000.00000000","50000.00000000","776.26939000",1725454799999,"43949985.04994390",93706,"363.62337000","20590941.14061460","0"],
+          [1725454800000,"50000.00000000","50000.00000000","50000.00000000","50000.00000000","1446.03410000",1725458399999,"81687899.57771600",192315,"740.04595000","41812275.07135860","0"]
+        ];
+
+        jest.spyOn(binanceService, 'fetchKLines').mockImplementation(async () => Promise.resolve(kLines));
+        const result = await analyzeHistoricalData({
+          symbol: BinanceSymbolEnum.BTC_USDT,
+          interval: BinanceIntervalEnum.ONE_HOUR,
+        })
+
+        expect(result).toEqual({
+          symbol: 'BTCUSDT',
+          interval: '1h',
+          timestampFrom: 1725451200000,
+          timestampTo: 1725458399999,
+          priceChangeDirection: 'equal',
+          priceChange: 0,
+          priceChangeInPercent: 0
         })
       })
     })
